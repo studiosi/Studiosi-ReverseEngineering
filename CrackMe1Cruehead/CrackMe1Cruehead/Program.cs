ff0218c6b364dfbbb6565be9d93c4218b0af6d80
David Gil de Gómez Pérez <dgil@uef.fi>
2016-12-27 19:38:26 +0200

    CrackMe v1.0 by Cruehead/MiB added
    Added the keygen for the CrackMe v1.0 by Cruehead/MiB

---
+++ CrackMe1Cruehead/CrackMe1Cruehead/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackMe1Cruehead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== Crackme v1.0 by Cruehead/MiB KeyGen by Studiosi ==\r\n");
+
+            // Get name
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            // Calculate name chksum
+            byte[] ascName = Encoding.ASCII.GetBytes(name);
+            UInt32 r1 = 0;
+            foreach(byte c in ascName)
+            {
+                if(c < 0x41)
+                {
+                    Console.WriteLine("Invalid Name (only ASCII > 0x41 chars)");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    byte x = c;
+                    if (x >= 0x5A)
+                        x = (byte)(x - 0x20);
+                    r1 += x;                
+                }
+            }
+            r1 = r1 ^ 0x5678;
+
+            // Search proper valid serial
+            UInt32 r2 = r1 ^ 0x1234; // XOR is a reversible op
+            Console.Write("Serial: ");
+            Console.WriteLine("{0}", r2);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CrackMe1Cruehead/CrackMe1Cruehead/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrackMe1Cruehead")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrackMe1Cruehead")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88c0a34e-5b4e-4951-a92c-f8dc9ed29f57")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
